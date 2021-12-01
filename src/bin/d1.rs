a6705f8a4406beef299e5b0e3d09dece9714afd7
Benjamin Herr <git@1d6.org>
2021-11-30 21:25:43 -0800

    rustfmt

--- src/bin/d1.rs
+++ src/bin/d1.rs
@@ -1,20 +1,20 @@
 use itertools::Itertools;
 
-use std::io::{self, BufReader, BufRead};
+use std::io::{self, BufRead, BufReader};
 
 fn main() {
-	let numbers: Vec<_> = BufReader::new(io::stdin()).lines()
-		.filter_map(|line|
-				line.ok()
-					.and_then(|line| line.trim().parse::<i64>().ok()))
-		.collect();
+    let numbers: Vec<_> = BufReader::new(io::stdin())
+        .lines()
+        .filter_map(|line| line.ok().and_then(|line| line.trim().parse::<i64>().ok()))
+        .collect();
 
-	let triples = numbers.iter().tuple_windows();
+    let triples = numbers.iter().tuple_windows();
 
-	let counter: i64 = triples.clone().zip(triples.clone().skip(1))
-		.map(|((a1, a2, a3), (b1, b2, b3))|
-			if a1+a2+a3 < b1+b2+b3 { 1 } else { 0 })
-		.sum();
+    let counter: i64 = triples
+        .clone()
+        .zip(triples.clone().skip(1))
+        .map(|((a1, a2, a3), (b1, b2, b3))| if a1 + a2 + a3 < b1 + b2 + b3 { 1 } else { 0 })
+        .sum();
 
-	println!("{}", counter);
+    println!("{}", counter);
 }
