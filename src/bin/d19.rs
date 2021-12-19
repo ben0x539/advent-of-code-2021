9f4dbfd47dd400f9c3885497ae5a6477d7865334
Benjamin Herr <git@1d6.org>
2021-12-19 02:59:06 -0800

    d19 refactor

--- src/bin/d19.rs
+++ src/bin/d19.rs
@@ -1,25 +1,179 @@
+use std::iter;
 use std::io::{self, BufRead, BufReader};
 use std::cmp::Ordering::*;
 use std::collections::BTreeSet;
+use std::ops;
 
 use eyre::{Result, WrapErr, eyre, bail};
 
-#[derive(Clone, Debug, PartialEq, PartialOrd, Eq, Ord)]
-struct Beacon {
-	coords: (i32, i32, i32),
-	neighbors: Vec<(i32, i32, i32)>,
+#[derive(PartialEq, PartialOrd, Eq, Ord, Debug, Clone, Copy)]
+struct Vec3 {
+	x: i32,
+	y: i32,
+	z: i32,
 }
 
-fn beacons_from_coords(coordses: &[(i32, i32, i32)]) -> Vec<Beacon> {
-	coordses.iter().map(|&coords@(x, y, z)| {
-		let mut neighbors: Vec<_> = coordses.iter().filter_map(|(x2,y2,z2)|
-			match (x2-x, y2-y, z2-z) {
-				(0, 0, 0) => None,
-				d => Some(d),
-			}).collect();
-		neighbors.sort();
+type Rotation = fn((i32, i32, i32)) -> (i32, i32, i32);
+
+impl Vec3 {
+	fn rotate(self, r: Rotation) -> Vec3 {
+		r(self.into()).into()
+	}
+}
+
+impl From<(i32, i32, i32)> for Vec3 {
+	fn from((x, y, z): (i32, i32, i32)) -> Vec3 { Vec3 { x, y, z } }
+}
+
+impl From<Vec3> for (i32, i32, i32) {
+	fn from(Vec3 { x, y, z }: Vec3) -> (i32, i32, i32) { (x, y, z) }
+}
+
+impl ops::Add for Vec3 {
+	type Output = Vec3;
+
+	fn add(self, rhs: Vec3) -> Vec3 {
+		Vec3 {
+			x: self.x + rhs.x,
+			y: self.y + rhs.y,
+			z: self.z + rhs.z,
+		}
+	}
+}
+
+impl ops::Sub for Vec3 {
+	type Output = Vec3;
+
+	fn sub(self, rhs: Vec3) -> Vec3 {
+		self + -rhs
+	}
+}
+
+impl ops::Neg for Vec3 {
+	type Output = Vec3;
+
+	fn neg(self) -> Vec3 {
+		Vec3 {
+			x: -self.x,
+			y: -self.y,
+			z: -self.z,
+		}
+	}
+}
+
+fn manhattan_distance(a: Vec3, b: Vec3) -> i32 {
+	fn d(a: i32, b: i32) -> i32 { i32::abs(a-b) }
+
+	d(a.x, b.x) + d(a.y, b.y) + d(a.z, b.z)
+}
+
+#[derive(Clone, Debug, PartialEq, PartialOrd, Eq, Ord)]
+struct Beacon {
+	coords: Vec3,
+	neighbors: Vec<Vec3>,
+}
+
+fn neighbors_for_beacon<I: Iterator<Item=Vec3>>(coords: Vec3, all: I)
+		-> Vec<Vec3> {
+	let mut neighbors: Vec<_> = all.filter_map(|other|
+		match coords == other {
+			true => None,
+			false => Some(other - coords),
+		}).collect();
+	neighbors.sort();
+	neighbors
+}
+
+fn beacons_with_neighbors(beacon_coords: &[Vec3]) -> Vec<Beacon> {
+	beacon_coords.iter().map(|&coords| {
+		let neighbors =
+			neighbors_for_beacon(coords, beacon_coords.iter().cloned());
 		Beacon { coords, neighbors }
 	}).collect()
+}
+
+const ROTATIONS: &[Rotation] = &[
+	|(x, y, z)| ( x,  y,  z),
+	|(x, y, z)| ( x,  z, -y), // rotation around +x
+	|(x, y, z)| ( x, -y, -z),
+	|(x, y, z)| ( x, -z,  y),
+
+	|(x, y, z)| (-x,  y, -z),
+	|(x, y, z)| (-x, -y,  z),
+	|(x, y, z)| (-x, -z, -y),
+	|(x, y, z)| (-x,  z,  y),
+
+	|(x, y, z)| ( y,  z,  x), 
+	|(x, y, z)| ( y, -z, -x),
+	|(x, y, z)| ( y,  x, -z),
+	|(x, y, z)| ( y, -x,  z), // rotation around +z
+
+	|(x, y, z)| (-y,  x,  z),
+	|(x, y, z)| (-y, -x, -z),
+	|(x, y, z)| (-y, -z,  x),
+	|(x, y, z)| (-y,  z, -x),
+
+	|(x, y, z)| ( z,  x,  y),
+	|(x, y, z)| ( z, -x, -y),
+	|(x, y, z)| ( z, -y,  x),
+	|(x, y, z)| ( z,  y, -x),
+
+	|(x, y, z)| (-z, -x,  y),
+	|(x, y, z)| (-z,  x, -y),
+	|(x, y, z)| (-z,  y,  x), // rotation around +y
+	|(x, y, z)| (-z, -y, -x),
+];
+
+// let permutations: &[fn((i32, i32, i32)) -> (i32, i32, i32)] = &[
+// 	|(x, y, z)| (x, y, z),
+// 	|(x, y, z)| (y, z, x),
+// 	|(x, y, z)| (z, x, y),
+// 	|(x, y, z)| (x, z, y),
+// 	|(x, y, z)| (z, y, x),
+// 	|(x, y, z)| (y, x, z),
+// ];
+
+// let flips: &[fn((i32, i32, i32)) -> (i32, i32, i32)] = &[
+// 	|(x, y, z)| (x, y, z),
+// 	|(x, y, z)| (x, y, -z),
+// 	|(x, y, z)| (x, -y, z),
+// 	|(x, y, z)| (x, -y, -z),
+// 	|(x, y, z)| (-x, y, z),
+// 	|(x, y, z)| (-x, y, -z),
+// 	|(x, y, z)| (-x, -y, z),
+// 	|(x, y, z)| (-x, -y, -z),
+// ];
+
+
+fn count_matches<'a, T, I>(xs: I, ys: I) -> u32
+		where T: 'a+Ord, I: IntoIterator<Item=&'a T> {
+	let mut xs = xs.into_iter();
+	let mut ys = ys.into_iter();
+	let mut x = xs.next();
+	let mut y = ys.next();
+	let mut count = 0;
+	loop {
+		match x.and_then(|x| y.map(|y| x.cmp(&y))) {
+			None => return count,
+			Some(Less) => x = xs.next(),
+			Some(Greater) => y = ys.next(),
+			Some(Equal) => {
+				x = xs.next();
+				y = ys.next();
+				count += 1;
+			}
+		}
+	}
+}
+
+fn retain_unordered<T, F: FnMut(&mut T) -> bool>(v: &mut Vec<T>, mut f: F) {
+	let mut i = 0;
+	while i < v.len() {
+		match f(&mut v[i]) {
+			true => { i += 1; }
+			false => { v.swap_remove(i); }
+		}
+	}
 }
 
 fn main() -> Result<()> {
@@ -34,181 +188,96 @@ fn main() -> Result<()> {
 			let mut coords = line.split(',')
 				.map(|s| s.parse::<i32>()
 					.with_context(|| eyre!("weird coord in {}", line)));
-			let coords = (
-				coords.next().ok_or_else(|| eyre!("missing coord"))??,
-				coords.next().ok_or_else(|| eyre!("missing coord"))??,
-				coords.next().ok_or_else(|| eyre!("missing coord"))??,
-			);
+			let coords = Vec3 {
+				x: coords.next().ok_or_else(|| eyre!("missing coord"))??,
+				y: coords.next().ok_or_else(|| eyre!("missing coord"))??,
+				z: coords.next().ok_or_else(|| eyre!("missing coord"))??,
+			};
 			scanner_beacon_coords
 				.last_mut().ok_or_else(|| eyre!("wtf no scanner"))?
 				.push(coords);
 		}
 	}
 
-	if scanner_beacon_coords.is_empty() {
-		bail!("no scanners, rip");
-	}
+	let mut remaining_areas: Vec<Vec<Beacon>> =
+		scanner_beacon_coords.iter().map(|beacon_coords|
+			beacons_with_neighbors(beacon_coords)).collect();
 
-	let mut scanner_beacons: Vec<Vec<Beacon>> =
-		scanner_beacon_coords.iter().map(|coordses|
-			beacons_from_coords(coordses)).collect();
-		//	coordses.iter().map(|&coords@(x, y, z)| {
-		//		let mut neighbors: Vec<_> = coordses.iter().map(|(x2,y2,z2)|
-		//			(x2-x, y2-y, z2-z)).collect();
-		//		neighbors.sort();
-		//		Beacon { coords, neighbors }
-		//	}).collect()
-		//}).collect();
+	let initial_area = remaining_areas.pop()
+		.ok_or_else(|| eyre!("no scanners, rip"))?;
 
-	let rx = |(x, y, z)| ( x,  z, -y), // rotation around +x
-	let ry = |(x, y, z)| (-z,  y,  x), // rotation around +y
-	let rz = |(x, y, z)| ( y, -x,  z), // rotation around +z
+	let mut normalized_beacons: BTreeSet<_> =
+		initial_area.iter().map(|beacon| beacon.coords).collect();
 
-	let rotations: &[fn((i32, i32, i32)) -> (i32, i32, i32)] = &[
-		|(x, y, z)| ( x,  y,  z),
-		|(x, y, z)| ( x,  z, -y), // rotation around +x
-		|(x, y, z)| ( x, -y, -z),
-		|(x, y, z)| ( x, -z,  y),
+	let mut reference_areas = vec![initial_area];
 
-		|(x, y, z)| (-x,  y, -z),
-		|(x, y, z)| (-x, -y,  z),
-		|(x, y, z)| (-x, -z, -y),
-		|(x, y, z)| (-x,  z,  y),
+	let mut normalized_scanners = vec![(0, 0, 0).into()];
 
-		|(x, y, z)| ( y,  z,  x), 
-		|(x, y, z)| ( y, -z, -x),
-		|(x, y, z)| ( y,  x, -z),
-		|(x, y, z)| ( y, -x,  z), // rotation around +z
+	while !remaining_areas.is_empty() {
+		//eprintln!("unconnected scanners left: {}", remaining_areas.len());
+		let mut new_normalized_areas = Vec::new();
 
-		|(x, y, z)| (-y,  x,  z),
-		|(x, y, z)| (-y, -x, -z),
-		|(x, y, z)| (-y, -z,  x),
-		|(x, y, z)| (-y,  z, -x),
+		retain_unordered(&mut remaining_areas, |candidate_area| {
+			for reference_beacon in reference_areas.iter().flatten() {
+				for candidate_beacon in &*candidate_area {
+					for &r in ROTATIONS {
+						let mut candidate_neighbors: Vec<_> =
+							candidate_beacon.neighbors.iter().cloned()
+								.map(|n| n.rotate(r)).collect();
+						candidate_neighbors.sort();
 
-		|(x, y, z)| ( z,  x,  y),
-		|(x, y, z)| ( z, -x, -y),
-		|(x, y, z)| ( z, -y,  x),
-		|(x, y, z)| ( z,  y, -x),
+						let matches = count_matches(
+							&reference_beacon.neighbors, &candidate_neighbors);
 
-		|(x, y, z)| (-z, -x,  y),
-		|(x, y, z)| (-z,  x, -y),
-		|(x, y, z)| (-z,  y,  x), // rotation around +y
-		|(x, y, z)| (-z, -y, -x),
-	];
-
-	// let permutations: &[fn((i32, i32, i32)) -> (i32, i32, i32)] = &[
-	// 	|(x, y, z)| (x, y, z),
-	// 	|(x, y, z)| (y, z, x),
-	// 	|(x, y, z)| (z, x, y),
-	// 	|(x, y, z)| (x, z, y),
-	// 	|(x, y, z)| (z, y, x),
-	// 	|(x, y, z)| (y, x, z),
-	// ];
-
-	// let flips: &[fn((i32, i32, i32)) -> (i32, i32, i32)] = &[
-	// 	|(x, y, z)| (x, y, z),
-	// 	|(x, y, z)| (x, y, -z),
-	// 	|(x, y, z)| (x, -y, z),
-	// 	|(x, y, z)| (x, -y, -z),
-	// 	|(x, y, z)| (-x, y, z),
-	// 	|(x, y, z)| (-x, y, -z),
-	// 	|(x, y, z)| (-x, -y, z),
-	// 	|(x, y, z)| (-x, -y, -z),
-	// ];
-
-	//for b in &scanner_beacons {
-	//	dbg!(b.len());
-	//}
-
-	let mut beacons_to_compare = scanner_beacons.pop().unwrap();
-	beacons_to_compare.sort();
-	let mut beacons_found: BTreeSet<_> =
-		beacons_to_compare.iter().map(|b| b.coords).collect();
-	let mut b2 =
-		beacons_from_coords(&beacons_found.iter().cloned().collect::<Vec<_>>());
-	b2.sort();
-	assert_eq!(beacons_to_compare, b2);
-	let mut scanners = vec![(0, 0, 0)];
-
-	while !scanner_beacons.is_empty() {
-		eprintln!("unconnected scanners left: {}", scanner_beacons.len());
-		let mut k = 0;
-		let mut success = false;
-		'beacons: while k < scanner_beacons.len() {
-			//dbg!(k, scanner_beacons.len(), beacons_to_compare.len());
-			//eprintln!("inner len {}", scanner_beacons.len());
-			let beacons2 = &scanner_beacons[k];
-
-			for beacon1 in &beacons_to_compare {
-				for beacon2 in beacons2 {
-					let (x, y, z) = beacon1.coords;
-					for r in rotations {
-						let mut neighbors2 = beacon2.neighbors.clone();
-						for c in &mut neighbors2 {
-							*c = r(*c)
+						if matches < 11 {
+							continue;
 						}
 
-						neighbors2.sort();
-						let mut overlap = Vec::new();
+						normalized_beacons.extend(
+							candidate_neighbors.iter()
+								.map(|&neighbor| reference_beacon.coords + neighbor));
 
-						let (mut i, mut j) = (0, 0);
-						while i < beacon1.neighbors.len() && j < neighbors2.len()  {
-							match beacon1.neighbors[i].cmp(&neighbors2[j]) {
-								Equal => {
-									overlap.push((i, j));
-									i += 1;
-									j += 1;
-								}
-								Less => i += 1,
-								Greater => j += 1,
-							}
-						}
+						let normalized_beacon_coords: Vec<_> =
+							candidate_neighbors.iter()
+								.map(|&c| c + reference_beacon.coords)
+								.chain(iter::once(reference_beacon.coords))
+								.collect();
+						new_normalized_areas.push(
+							beacons_with_neighbors(&normalized_beacon_coords));
 
-						if overlap.len() >= 11 {
-							let mut new_beacon_coords = neighbors2;
-
-							for &(dx, dy, dz) in &new_beacon_coords {
-								beacons_found.insert((x+dx, y+dy, z+dz));
-							}
-
-							let (x2, y2, z2) = beacon2.coords;
-							let (x2, y2, z2) = r((-x2, -y2, -z2));
-							scanners.push((x2+x, y2+y, z2+z));
-							scanner_beacons.swap_remove(k);
-							success = true;
-							continue 'beacons;
-						}
+						let normalized_coords = candidate_beacon.coords.rotate(r);
+						normalized_scanners.push(
+							reference_beacon.coords - normalized_coords);
+						return false;
 					}
 				}
 			}
-			k += 1;
+
+			return true;
+		});
+
+		if new_normalized_areas.is_empty() {
+			bail!("rip, {} unmatched scanners left", remaining_areas.len());
 		}
 
-		if !success {
-			bail!("rip, {} unmatched scanners left", scanner_beacons.len());
-		}
-
-		beacons_to_compare =
-			beacons_from_coords(&beacons_found.iter().cloned().collect::<Vec<_>>());
-		//dbg!(beacons_found.len());
+		reference_areas = new_normalized_areas;
 	}
 
-	for (i, (x, y, z)) in beacons_found.iter().enumerate() {
-		println!("beacon {i:>3}:   {x:>5}, {y:>5}, {z:>5}");
-	}
+	// for (i, &Vec3 { x, y, z }) in normalized_beacons.iter().enumerate() {
+	// 	println!("beacon {i:>3}:   {x:>5}, {y:>5}, {z:>5}");
+	// }
 
-	for (i, (x, y, z)) in scanners.iter().enumerate() {
-		println!("scanner {i:>3}:   {x:>5}, {y:>5}, {z:>5}");
-	}
+	// for (i, &Vec3 { x, y, z }) in normalized_scanners.iter().enumerate() {
+	// 	println!("scanner {i:>3}:   {x:>5}, {y:>5}, {z:>5}");
+	// }
 
-	println!("beacons found: {}", beacons_found.len());
-	println!("scanners found: {}", scanners.len());
+	println!("beacons found: {}", normalized_beacons.len());
+	//println!("scanners found: {}", normalized_scanners.len());
 
-	fn d(a: i32, b: i32) -> i32 { i32::abs(a-b) }
-
-	let max_distance = scanners.iter().flat_map(|&(x1, y1, z1)|
-		scanners.iter().map(move |&(x2, y2, z2)|
-			d(x1, x2) + d(y1, y2) + d(z1, z2))).max().unwrap();
+	let max_distance = normalized_scanners.iter().flat_map(|&s1|
+			normalized_scanners.iter().map(move |&s2|
+				manhattan_distance(s1, s2)))
+		.max().unwrap();
 	println!("max distance: {max_distance}");
 
 	Ok(())
